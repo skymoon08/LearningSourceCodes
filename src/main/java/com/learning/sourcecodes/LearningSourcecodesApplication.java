cae73cc4f9fb386d26951e2cb1c7044a0c5af2c9
skymoon08 <zhangsunny0808@gmail.com>
2019-10-24 22:18:28 +0800

    first commit

---
+++ src/main/java/com/learning/sourcecodes/LearningSourcecodesApplication.java
@@ -0,0 +1,13 @@
+package com.learning.sourcecodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSourcecodesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSourcecodesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/sourcecodes/LearningSourcecodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.sourcecodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSourcecodesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
